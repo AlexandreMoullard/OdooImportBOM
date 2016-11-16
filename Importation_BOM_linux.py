3feed4404449ad522da3fb57540e621b5ed70131
alex <alexandre.moullard@gmail.com>
2016-11-16 02:09:31 +0100

    Added supplier info table for importation Issue on product_product to correct

--- Importation_BOM_linux.py
+++ Importation_BOM_linux.py
@@ -29,14 +29,23 @@ with open('products.csv', 'r') as f:
     for row in reader :
         print (row[1])
     
-        #import routine
-        statement = "INSERT INTO product_template(name,warranty,list_price,mes_type,weight,write_uid,uos_coeff,create_uid,company_id,uom_id,uom_po_id,type,weight_net,volume,active,rental,track_all,track_outgoing,track_incoming,sale_delay,track_production,produce_delay,categ_id,sale_ok,create_date,write_date) VALUES ('"+ row[1] + "',0," + str(row[2])  +",'fixed',0.00,1,1.000,1,1,1,1,'product',0.00,0,TRUE,FALSE,FALSE,FALSE,FALSE,7,FALSE,1,1,True,'"+str(time)+"','"+str(time)+"') RETURNING id"
+        #import routine product_template
+        statement = "INSERT INTO product_template(name,warranty,list_price,mes_type,weight,write_uid,uos_coeff,create_uid,company_id,uom_id,uom_po_id,type,weight_net,volume,active,rental,track_all,track_outgoing,track_incoming,sale_delay,track_production,produce_delay,categ_id,sale_ok,create_date,write_date) VALUES ('"+ row[1] + "',0," + str(row[2])  +",'fixed',0.00,1,1.000,1,1,1,1,'product',0.00,0,TRUE,FALSE,FALSE,FALSE,FALSE,7,FALSE,1,1,True,'"+ str(time) +"','"+ str(time) +"') RETURNING id"
+
+        cursor.execute(statement)
+        conn.commit()
+        templateid = cursor.fetchone()[0]
+
+	#import routine product_supplierinfo -> to be completed
+	statement = "INSERT INTO product_supplierinfo(create_uid,product_code,create_date,name,sequence,product_name,company_id,write_id,delay,write_date,min_qty,qty,product_tmpl_id) VALUES (1,,'"+ str(time) +"',,1,,1,1,1,'"+ str(time) +"',0,0.00,)"
 
         cursor.execute(statement)
         conn.commit()
         templateid = cursor.fetchone()[0]
     
-        statement = "INSERT INTO product_product (product_tmpl_id,default_code,active) VALUES \
+	#import routine product_product -> issue, has to be corrected
+	#replace by  (create_date,name_template,create_uid,product_tmpl_id,write_uid,write_date,active)        
+	statement = "INSERT INTO product_product (product_tmpl_id,default_code,active) VALUES \
         (" + str(templateid) + ",'" + row[0] + "',TRUE)"
     
         cursor.execute(statement)
